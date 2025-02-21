ab7014d48d7c198570ff58d364e39ec0b8d83fc9
Pokio Project <pokiochain@gmail.com>
2025-02-21 03:45:22 +0100

    interface improvements
    - expected blocktime
    - corrected indentation

--- src/main.rs
+++ src/main.rs
@@ -15,85 +15,85 @@ use ethereum_types::U256;
 use chrono::Local;
 
 fn hash_func(args: &[&[u8]]) -> Vec<u8> {
-    let mut hasher = Sha256::new();
-    for arg in args {
-        hasher.update(arg);
-    }
-    hasher.finalize().to_vec()
+	let mut hasher = Sha256::new();
+	for arg in args {
+		hasher.update(arg);
+	}
+	hasher.finalize().to_vec()
 }
 
 fn expand(buf: &mut Vec<Vec<u8>>, space_cost: usize) {
-    for s in 1..space_cost {
-        let new_hash = hash_func(&[&buf[s - 1]]);
-        buf.push(new_hash);
-    }
+	for s in 1..space_cost {
+		let new_hash = hash_func(&[&buf[s - 1]]);
+		buf.push(new_hash);
+	}
 }
 
 fn mix(buf: &mut Vec<Vec<u8>>, delta: usize, salt: &[u8], space_cost: usize, time_cost: usize) {
-    for _ in 0..time_cost {
-        for s in 0..space_cost {
-            let prev = buf[s.saturating_sub(1)].clone();
-            buf[s] = hash_func(&[&prev, &buf[s]]);
-            
-            for i in 0..delta {
-                let idx_block = hash_func(&[salt, &i.to_le_bytes()]);
-                let other = usize::from_le_bytes(idx_block[..8].try_into().unwrap()) % space_cost;
-                buf[s] = hash_func(&[&buf[s], &buf[other]]);
-            }
-        }
-    }
+	for _ in 0..time_cost {
+		for s in 0..space_cost {
+			let prev = buf[s.saturating_sub(1)].clone();
+			buf[s] = hash_func(&[&prev, &buf[s]]);
+			
+			for i in 0..delta {
+				let idx_block = hash_func(&[salt, &i.to_le_bytes()]);
+				let other = usize::from_le_bytes(idx_block[..8].try_into().unwrap()) % space_cost;
+				buf[s] = hash_func(&[&buf[s], &buf[other]]);
+			}
+		}
+	}
 }
 
 fn extract(buf: &Vec<Vec<u8>>) -> Vec<u8> {
-    buf.last().unwrap().clone()
+	buf.last().unwrap().clone()
 }
 
 pub fn pokiohash(password: &str, salt: &str, space_cost: usize, time_cost: usize, delta: usize) -> Vec<u8> {
-    let salt_bytes = salt.as_bytes();
-    let mut buf = vec![hash_func(&[password.as_bytes(), salt_bytes])];
-    
-    expand(&mut buf, space_cost);
-    mix(&mut buf, delta, salt_bytes, space_cost, time_cost);
-    extract(&buf)
+	let salt_bytes = salt.as_bytes();
+	let mut buf = vec![hash_func(&[password.as_bytes(), salt_bytes])];
+	
+	expand(&mut buf, space_cost);
+	mix(&mut buf, delta, salt_bytes, space_cost, time_cost);
+	extract(&buf)
 }
 
 pub fn pokiohash_hash(password: &str, salt: &str) -> String {
-    let hash_bytes = pokiohash(password, salt, 16, 20, 4);
-    hex::encode(hash_bytes)
+	let hash_bytes = pokiohash(password, salt, 16, 20, 4);
+	hex::encode(hash_bytes)
 }
 
 fn hash_to_difficulty(hash: &str) -> U256 {
-    let hash_value = U256::from_str_radix(hash, 16).unwrap_or(U256::zero());
-    let max_value = U256::MAX;
-    let difficulty = max_value / hash_value;
-    difficulty
+	let hash_value = U256::from_str_radix(hash, 16).unwrap_or(U256::zero());
+	let max_value = U256::MAX;
+	let difficulty = max_value / hash_value;
+	difficulty
 }
 
 fn mine(hash_count: Arc<Mutex<u64>>, password: Arc<Mutex<String>>, wallet: String, pserver: String) {
-    let mut rng = rand::thread_rng();
+	let mut rng = rand::thread_rng();
 	let base_url = pserver + "mine.php?";
-    loop {
-        let nonce: u64 = rng.gen();
-        let salt = format!("{:016x}", nonce);
+	loop {
+		let nonce: u64 = rng.gen();
+		let salt = format!("{:016x}", nonce);
 
-        let password = password.lock().unwrap().clone();
+		let ppassword = password.lock().unwrap().clone();
 		
-		let parts: Vec<&str> = password.split('-').collect();
+		let parts: Vec<&str> = ppassword.split('-').collect();
 		let tdiff: u64 = parts[2].parse().unwrap_or(0);
 		let h: u64 = parts[0].parse().unwrap_or(0);
 		let coins: u64 = parts[3].parse().unwrap_or(0);
 		let target_diff = U256::from(tdiff);
 		
-        let hash = pokiohash_hash(&password, &salt);
-        let difficulty = hash_to_difficulty(&hash) as U256;
+		let hash = pokiohash_hash(&ppassword, &salt);
+		let difficulty = hash_to_difficulty(&hash) as U256;
 
-        {
-            let mut count = hash_count.lock().unwrap();
-            *count += 1;
-        }
+		{
+			let mut count = hash_count.lock().unwrap();
+			*count += 1;
+		}
 
-        if difficulty > target_diff {
-            println!("{} Block found. Nonce: {}, Hash: {}", Local::now().format("[%H:%M:%S]").to_string(), salt, hash);
+		if difficulty > target_diff {
+			println!("{} Block found. Nonce: {}, Hash: {}", Local::now().format("[%H:%M:%S]").to_string(), salt, hash);
 			
 			let url = Serializer::new(base_url.to_string())
 				.append_pair("height", &h.to_string())
@@ -111,21 +111,20 @@ fn mine(hash_count: Arc<Mutex<u64>>, password: Arc<Mutex<String>>, wallet: Strin
 				}
 				Err(e) => println!("{} Error getting template: {}", Local::now().format("[%H:%M:%S]").to_string(), e),
 			}
-        }
-    }
+		}
+	}
 }
 
 fn fetch_password(password: Arc<Mutex<String>>, hash_count: Arc<Mutex<u64>>, pserver: String) {
 	let start_time = Instant::now();
 	let base_url = pserver + "/template.php?";
-    loop {
-		let tocoins = (*hash_count.lock().unwrap() as f64 / start_time.elapsed().as_secs_f64().round()) as u64;
-		let tocoins = max(1, (tocoins / 100) + 1);
+	loop {
+		let hr = (*hash_count.lock().unwrap() as f64 / start_time.elapsed().as_secs_f64().round()) as u64;
+		let tocoins = max(1, (hr / 100) + 1);
 
 		let tocoins_str;
 
 		if start_time.elapsed().as_secs_f64() < 10.0 {
-			println!("{} time elapsed: {}", Local::now().format("[%H:%M:%S]"), start_time.elapsed().as_secs_f64());
 			tocoins_str = "10000".to_string();
 		} else {
 			tocoins_str = tocoins.to_string();
@@ -137,111 +136,131 @@ fn fetch_password(password: Arc<Mutex<String>>, hash_count: Arc<Mutex<u64>>, pse
 
 
 		match reqwest::blocking::get(&url) {
-            Ok(response) => {
-                if let Ok(new_password) = response.text() {
-                    let mut password_lock = password.lock().unwrap();
+			Ok(response) => {
+				if let Ok(new_password) = response.text() {
+					let mut password_lock = password.lock().unwrap();
 					
 					let new_password_cmp = new_password.trim().to_string();
 					if new_password_cmp.trim().to_string() != *password_lock
 					{
 						*password_lock = new_password.trim().to_string();
-						println!("{} New block template received: {}", Local::now().format("[%H:%M:%S]").to_string(), *password_lock);
+						let npassword = new_password.trim().to_string();
+						
+						let parts: Vec<&str> = npassword.split('-').collect();
+						let ntdiff: u64 = parts[2].parse().unwrap_or(0);
+						let nh: u64 = parts[0].parse().unwrap_or(0);
+						let ncoins: u64 = parts[3].parse().unwrap_or(0);
+						
+						println!("{} New template with height {}, diff: {}, target: {} POKIO", Local::now().format("[%H:%M:%S]").to_string(), nh, ntdiff, ncoins );
+						
+						if start_time.elapsed().as_secs_f64() > 10.0 {
+							println!("{} Expected mining rate: {} secs/block", Local::now().format("[%H:%M:%S]").to_string(), ntdiff / hr);
+						}
+						
 					}
-                }
-            }
-            Err(e) => println!("{} Error getting template: {}", Local::now().format("[%H:%M:%S]").to_string(), e),
-        }
+				}
+			}
+			Err(e) => println!("{} Error getting template: {}", Local::now().format("[%H:%M:%S]").to_string(), e),
+		}
 
-        thread::sleep(Duration::from_secs(10));
-    }
+		thread::sleep(Duration::from_secs(10));
+	}
 }
 
 fn main() {
-    let mut sys = System::new_all();
-    sys.refresh_all();
+	let args: Vec<String> = env::args().collect();
+	let num_threads = args.iter().position(|arg| arg == "--t")
+		.and_then(|i| args.get(i + 1))
+		.and_then(|t| t.parse::<usize>().ok())
+		.unwrap_or(1);
+		
+	let wallet = args.iter().position(|arg| arg == "--w")
+		.and_then(|i| args.get(i + 1))
+		.map(|s| s.to_string())
+		.unwrap_or_else(|| "default_wallet".to_string());
+		
+	let server = args.iter().position(|arg| arg == "--o")
+		.and_then(|i| args.get(i + 1))
+		.map(|s| s.to_string())
+		.unwrap_or_else(|| "https://pokio.xyz/".to_string());
+		
+	if wallet == "default_wallet" {
+		println!("Usage: pokiominer.exe --w your_wallet_address [OPTIONS]");
+		println!();
+		println!("Required:");
+		println!("  --w wallet	  Provide your wallet address.");
+		println!();
+		println!("Options:");
+		println!("  --o server_url  Specify the server URL to connect to. (default: https://pokio.xyz/)");
+		println!("  --t threads	 Set the number of threads to use (default: 1).");
+		println!();
+		println!("Example:");
+		println!("  pokiominer.exe --w your_wallet_address --t 4");
+		process::exit(0);
+	}
 	
-	env::set_var("TERM", "xterm-256color");
+	let mut sys = System::new_all();
+	sys.refresh_all();
 
 	println!("{}", "WARNING: EXPERIMENTAL MINER IN USE");
 	println!("{}", "\nYou are currently using the *experimental miner* for the Pokio project. Please note that this miner is strictly for pre-testnet testing purposes only. Any coins mined during this session have NO monetary value and will NOT be transferred to the mainnet.\n");
 	println!("{}", "We strongly advise limiting your use of this miner to no more than 30 minutes, just enough to send the necessary statistics and ensure everything is functioning correctly. This will greatly assist us in fine-tuning the system. Your participation, though temporary, is invaluable to the project's progress.\n");
 	println!("{}", "Thank you for helping us improve the Pokio experience!\n");
 
-    let total_memory = sys.total_memory() / 1024 / 1024;
-    let cpu_model = sys.cpus().get(0).map(|cpu| cpu.brand().to_string()).unwrap_or("Unknown".to_string());
+	let total_memory = sys.total_memory() / 1024 / 1024;
+	let cpu_model = sys.cpus().get(0).map(|cpu| cpu.brand().to_string()).unwrap_or("Unknown".to_string());
 
-    println!("Total RAM: {}",  format!("{} MB", total_memory));
+	println!("Total RAM: {}",  format!("{} MB", total_memory));
 	println!("CPU Model: {}\n", cpu_model.trim());
 
-    let args: Vec<String> = env::args().collect();
-    let num_threads = args.iter().position(|arg| arg == "--t")
-        .and_then(|i| args.get(i + 1))
-        .and_then(|t| t.parse::<usize>().ok())
-        .unwrap_or(1);
-		
-	let wallet = args.iter().position(|arg| arg == "--w")
-        .and_then(|i| args.get(i + 1))
-        .map(|s| s.to_string())
-        .unwrap_or_else(|| "default_wallet".to_string());
-		
-	let server = args.iter().position(|arg| arg == "--o")
-        .and_then(|i| args.get(i + 1))
-        .map(|s| s.to_string())
-        .unwrap_or_else(|| "https://pokio.xyz/".to_string());
-		
-	if wallet == "default_wallet" {
-        println!("\nNo wallet.\n\nrun: pokiominer.exe --w YOU_WALLET");
-        process::exit(0);
-    }
+	println!("{} Start mining with {} threads...", Local::now().format("[%H:%M:%S]").to_string(), num_threads);
 
-    println!("{} Start mining with {} threads...", Local::now().format("[%H:%M:%S]").to_string(), num_threads);
+	let hash_count = Arc::new(Mutex::new(0));
+	let start_time = Instant::now();
 
-    let hash_count = Arc::new(Mutex::new(0));
-    let start_time = Instant::now();
+	// template to mine
+	let password = Arc::new(Mutex::new("0-0-1000-10000".to_string()));
 
-    // template to mine
-    let password = Arc::new(Mutex::new("0-0-1000-10000".to_string()));
+	let mut handles = vec![];
 
-    let mut handles = vec![];
-
-    // updater thread
-    let password_clone = Arc::clone(&password);
+	// updater thread
+	let password_clone = Arc::clone(&password);
 	let hash_count_clone = Arc::clone(&hash_count);
 	
 	let pserver = server.clone();
-    handles.push(thread::spawn(move || {
-        fetch_password(password_clone, hash_count_clone, pserver);
-    }));
+	handles.push(thread::spawn(move || {
+		fetch_password(password_clone, hash_count_clone, pserver);
+	}));
 	
 	thread::sleep(Duration::from_secs(2));
 
-    // mining threads
-    for _ in 0..num_threads {
-        let hash_count_clone = Arc::clone(&hash_count);
-        let password_clone = Arc::clone(&password);
+	// mining threads
+	for _ in 0..num_threads {
+		let hash_count_clone = Arc::clone(&hash_count);
+		let password_clone = Arc::clone(&password);
 		let pwallet = wallet.clone();
 		let pserver = server.clone();
-        handles.push(thread::spawn(move || {
-            mine(hash_count_clone, password_clone, pwallet, pserver);
-        }));
-    }
+		handles.push(thread::spawn(move || {
+			mine(hash_count_clone, password_clone, pwallet, pserver);
+		}));
+	}
 
-    // i/o thread
-    let hash_count_clone = Arc::clone(&hash_count);
-    thread::spawn(move || {
-        loop {
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            if input.trim() == "h" {
-                let elapsed_secs = start_time.elapsed().as_secs_f64();
-                let total_hashes = *hash_count_clone.lock().unwrap();
-                let hashrate = total_hashes as f64 / elapsed_secs;
-                println!("{} Total Hashrate: {:.2} H/s", Local::now().format("[%H:%M:%S]").to_string(), hashrate);
-            }
-        }
-    });
+	// i/o thread
+	let hash_count_clone = Arc::clone(&hash_count);
+	thread::spawn(move || {
+		loop {
+			let mut input = String::new();
+			io::stdin().read_line(&mut input).unwrap();
+			if input.trim() == "h" {
+				let elapsed_secs = start_time.elapsed().as_secs_f64();
+				let total_hashes = *hash_count_clone.lock().unwrap();
+				let hashrate = total_hashes as f64 / elapsed_secs;
+				println!("{} Total Hashrate: {:.2} H/s", Local::now().format("[%H:%M:%S]").to_string(), hashrate);
+			}
+		}
+	});
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
+	for handle in handles {
+		handle.join().unwrap();
+	}
 }
